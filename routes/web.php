6d0102820d8c3d53a0273dcc772fe116f2a7340b
Edward Javier Pineda Moran <edwardjpineda@outlook.com>
2025-04-02 10:20:17 -0600

    Add routes and folders

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductReportController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SaleDetailController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('login');
@@ -28,6 +30,7 @@ Route::prefix('sales')->middleware('auth')->group(function(){
 });
 
 Route::prefix('salesdetails')->middleware('auth')->group(function(){
+    Route::get('/detalle-venta', [SaleDetailController::class, 'index'])->name('sale-details');
 });
 
 Route::prefix('categories')->middleware('auth')->group(function(){
@@ -73,5 +76,15 @@ Route::prefix('products')->middleware('auth')->group(function(){
 Route::prefix('products_report')->middleware('auth')->group(function(){
 });
 
+Route::prefix('graphics')->middleware('auth')->group(function(){
+});
+
 Route::prefix('users')->middleware('auth')->group(function(){
+    Route::get('/', [UserController::class, 'index'])->name('users');
+    Route::get('/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('/edit{id}', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/update{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/show{id}', [UserController::class, 'show'])->name('users.show');
+    Route::delete('/destroy{id}', [UserController::class, 'destroy'])->name('users.destroy');
 });
